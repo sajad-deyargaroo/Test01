15591a410413fdef291a577dec77d0e26822bf0a
sajad deyargaroo <sajad.deyargaroo@appliedis.com>
2016-04-22 13:44:59 +0000

    updated text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello docker, how are you today!');
+  res.send('hello docker, how are you today during demo!');
 });
  
 app.listen(process.env.PORT || 5000);
